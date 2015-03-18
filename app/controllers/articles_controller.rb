7358ee7a142607520b9bf093236b6cb6ca05f3a6
Chris Legg <chris@chrislegg.net>
2015-03-18 21:13:55 +0000

    Edit and delete articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: [:show, :edit, :destroy]
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   
   def index
@@ -26,6 +26,23 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @article.update(articles_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
 
   private
 
